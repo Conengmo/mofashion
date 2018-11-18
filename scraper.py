1177f54dd15afb74794af10b9e049813dab4615e
Frank <33519926+Conengmo@users.noreply.github.com>
2018-11-18 19:38:26 +0100

    Add Mango scraping

--- scraper.py
+++ scraper.py
@@ -7,6 +7,7 @@ import os
 import uuid
 import contextlib
 from typing import List, Optional
+import time
 
 from selenium import webdriver
 import requests
@@ -53,18 +54,26 @@ class ImageDownloader:
     def download_image(self,
                        image_url: str,
                        image_id: Optional[str] = None,
-                       empty_image: Optional[bytes] = None):
+                       empty_image: Optional[bytes] = None,
+                       raise_exc: bool = True):
+        if image_id is None:
+            image_id = uuid.uuid4()
+        filepath = os.path.join(self.path, f'{self.run_name}_{image_id}.jpeg')
+        if os.path.exists(filepath):
+            return
         resp = requests.get(image_url)
-        resp.raise_for_status()
+        if resp.status_code != 200:
+            if not raise_exc:
+                return
+            resp.raise_for_status()
         file_type, extension = resp.headers['Content-Type'].split('/')
-        assert file_type == 'image'
-        assert extension in ('jpeg',)
+        if file_type != 'image' or extension != 'jpeg':
+            if not raise_exc:
+                return
+            raise ValueError('Image HTTP headers have unexpected content type.')
         if empty_image is not None:
             if empty_image == resp.content:
                 return
-        if image_id is None:
-            image_id = uuid.uuid4()
-        filepath = os.path.join(self.path, f'{self.run_name}_{image_id}.{extension}')
         with open(filepath, 'wb') as f:
             f.write(resp.content)
 
@@ -151,6 +160,75 @@ def main_uniqlo(gender: str):
                 product_ids.append(product_id)
 
 
+def main_mango(gender: str):
+    if gender == 'm':
+        base_url = 'https://shop.mango.com/nl/heren/'
+        url_suffixs = ['t-shirts_c12018147', 'jassen_c32859776', 'jasjes_c16042202',
+                       'blazers_c14858698', 'truien-en-vesten_c12660076', 'sweaters_c71156082',
+                       'overhemden_c10863844', 'broeken_c11949748', 'jeans_c23998484']
+    elif gender == 'f':
+        # I'm skipping the large sizes section called violeta
+        base_url = 'https://shop.mango.com/nl/dames/'
+        url_suffixs = ['vesten-en-truien_c18200786', 'jassen_c67886633', 'jasjes_c69427016',
+                       'jurken_c55363448', 'jumpsuits_c99834840',
+                       'blouses_c78920337', 't-shirts-en-tops_c66796663',
+                       'broeken_c52748027', 'jeans_c99220156',
+                       'rokken_c20673898']
+    else:
+        raise ValueError('gender argument may be either \'m\' or \'f\'.')
+    url_image = ('https://st.mngbcn.com/rcs/pics/static/T3/fotos/S20/'
+                 '{product_id}_{c_code}{suffix}.jpg?ts={timestamp}&imwidth=427&imdensity=1')
+
+    dl_models_full = ImageDownloader(f'ma-{gender}-models-full')
+    dl_models_partial = ImageDownloader(f'ma-{gender}-models-partial')
+    dl_items_full = ImageDownloader(f'ma-{gender}-items-full')
+
+    full_urls = set()
+
+    with yield_driver() as driver:
+        for url_suffix in url_suffixs:
+            driver.get(base_url + url_suffix)
+            time.sleep(2)
+            driver.find_element_by_id('navColumns4').click()
+            time.sleep(2)
+            while True:
+                time.sleep(2)
+                item_urls = [item.get_attribute('src')
+                             for item in driver.find_elements_by_class_name('product-list-image')]
+                print(f'{url_suffix}: got {len(item_urls)} elements, total is {len(full_urls)}.')
+                len_full_urls = len(full_urls)
+                for item_url_full in item_urls:
+                    if item_url_full in full_urls:
+                        continue
+                    item_url = item_url_full.split('?')[0]
+                    if '.gif' in item_url:
+                        continue
+                    parts_str = item_url.rstrip('.jpg').split('/')[-1]
+                    parts = parts_str.split('_')
+                    product_id = int(parts[0])
+                    c_code = parts[1].split('-')[0]
+
+                    def call_download(dl_, suffix_):
+                        dl_.download_image(url_image.format(product_id=product_id, c_code=c_code,
+                                                            suffix=suffix_,
+                                                            timestamp=int(time.time())),
+                                           image_id=f'{product_id}{suffix_}',
+                                           raise_exc=False)
+
+                    for suffix in ('', '_R'):
+                        call_download(dl_models_full, suffix)
+                    for suffix in ('_D1', '_D2'):
+                        call_download(dl_models_partial, suffix)
+                    call_download(dl_items_full, '_B')
+                    full_urls.add(item_url_full)
+                    diff = len(full_urls) - len_full_urls
+                    if diff > 0 and diff % 4 == 0:
+                        driver.execute_script("window.scrollBy(0,600);")
+                if len(full_urls) == len_full_urls:
+                    print('End of page')
+                    break
+
+
 # def main_zalando():
 #     available = 892
 #     # done until page 45
@@ -167,6 +245,8 @@ def main_uniqlo(gender: str):
 if __name__ == '__main__':
     # main_hm(gender='m')
     # main_hm(gender='f')
-    main_uniqlo(gender='m')
-    main_uniqlo(gender='f')
+    # main_uniqlo(gender='m')
+    # main_uniqlo(gender='f')
+    main_mango(gender='m')
+    main_mango(gender='f')
     # main_zalando()
